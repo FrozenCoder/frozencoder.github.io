8770772f4e7255c46f96a9cd86428bca614f3776
Jason Johnson <jjohnson@lokion.com>
2016-04-04 13:47:34 -0500

    added JSON.stringify to msg

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ var win = document.getElementById("iframe");
 
   document.forms.form.onsubmit = function() {
     var msg = document.forms[0].elements["msg"].value;
-    win.contentWindow.postMessage(msg,"http://frozencoder.github.io");
+    win.contentWindow.postMessage(JSON.stringify(msg),"http://frozencoder.github.io");
     return false;
   }
 
-  function listener(event){
+function listener(event){
     //add this after I can make it work
-//  if ( event.origin !== "https://fiddle.jshell.net" )
+//  if ( event.origin !== "" )
   //  return
   console.log("message being received value = " + event.data);
-  document.getElementById("test").innerHTML = "received: " + event.data;
+  document.getElementById("test").innerHTML = "received: " + event.data
 }
 
 if (window.addEventListener){
